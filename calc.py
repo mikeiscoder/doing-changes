9f527184aca362da00aa2f330e5a18555b16ed55
mike <mike@example.com>
2025-07-09 16:17:56 +0530

    changes

--- calc.py
+++ calc.py
@@ -1,15 +1,9 @@
 a=int(input("Enter 1st number"))
 b=int(input("Enter 2nd number"))
 
-def add(a, b):
-    return a + b       
+print("Addition:", a + b)
+print("Subtraction:",  a - b)
 
-def subtract(a, b):
-    return a - b    
 
-addition = add(a, b)
-subtraction = subtract(a, b)
-print("Addition:", addition)
-print("Subtraction:", subtraction)
 
 
